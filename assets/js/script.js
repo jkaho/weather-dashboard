5aa96d0025af8ad364d482fac9ae80c7a7a9d4da
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-18 23:59:11 +1100

    Modify 'script.js' to test weather and forecast data ajax calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,28 @@
 // Grab value of search bar input to use as query in API query URL 
 
 // AJAX call using query URL for current weather
+    // Current weather data: city name, date, temp, humidity, wind speed, UV index
 
-// Current weather data: city name, date, temp, humidity, wind speed, UV index
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=sydney&units=metric&appid=21cf2c282545a0fc1251a4061d71efec"
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(response);
+})
 
 // AJAX call using query URL for forecast weather
+    // Forecast data: date, icon, min & max temp, humidity 
 
-// Forecast data: date, icon, min & max temp, humidity 
+var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=sydney&units=metric&appid=21cf2c282545a0fc1251a4061d71efec"
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(response);
+})
 
 // Add city to ul (event listener on each li to display weather data)
 
