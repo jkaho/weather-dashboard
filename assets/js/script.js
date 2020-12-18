d06b0046b1ed8596b7c3e2fa3a4fb761e6bcb532
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-18 23:12:57 +1100

    Modify 'script.js' to add pseudocode for app functionality

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Grab value of search bar input to use as query in API query URL 
+
+// AJAX call using query URL for current weather
+
+// Current weather data: city name, date, temp, humidity, wind speed, UV index
+
+// AJAX call using query URL for forecast weather
+
+// Forecast data: date, icon, min & max temp, humidity 
+
+// Add city to ul (event listener on each li to display weather data)
+
+// Local storage to save ul cities
+
+// Save last searched city into variable, display info upon opening page
