eb9c63174daa3759ea776f4b0ce0e7da5c8d1a8f
jkaho <72959522+jkaho@users.noreply.github.com>
2020-12-19 15:11:18 +1100

    Modify 'script.js' and 'index.html' to add click functionality to search btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,29 +3,35 @@
 // AJAX call using query URL for current weather
     // Current weather data: city name, date, temp, humidity, wind speed, UV index
 
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=sydney&units=metric&appid=21cf2c282545a0fc1251a4061d71efec"
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response);
-})
-
 // AJAX call using query URL for forecast weather
     // Forecast data: date, icon, min & max temp, humidity 
-
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=sydney&units=metric&appid=21cf2c282545a0fc1251a4061d71efec"
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response);
-})
 
 // Add city to ul (event listener on each li to display weather data)
 
 // Local storage to save ul cities
 
 // Save last searched city into variable, display info upon opening page
+
+$("#search-btn").on("click", function(event) {
+    event.preventDefault();
+
+    var searchWord = $("#search-word").val();
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchWord + "&units=metric&appid=21cf2c282545a0fc1251a4061d71efec"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    })
+
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + searchWord + "&units=metric&appid=21cf2c282545a0fc1251a4061d71efec"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+    })
+})
+
